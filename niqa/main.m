088b9d6f65001c5ee8e4a5018c92905ae192c44a
kevin_desktop <nature2575@yeah.net>
2013-07-25 22:52:11 +0800

    变差差分法评价原图像质量
    参考文献“The blur effect: perception and estimation with a new no-reference
    perceptual blur metric”

---
+++ niqa/main.m
@@ -0,0 +1,14 @@
+pth = 'D:\Backup\ÎÒµÄÎÄµµ\MATLAB\niqa\gblur';
+d = dir(pth);
+d = d(3:end);
+n = length(d);
+score = zeros(1,n);
+
+for i = 1:n
+    im = imread([pth '\img' num2str(i) '.bmp']);
+    im = im2double(rgb2gray(im));
+    score(i) = blurPerception(im);
+end
+save('subScores.mat','score');
+load dmos145;
+cftool(dmos145,score);
