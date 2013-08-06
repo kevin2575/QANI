c962c68d78df04e230ec4c0eaa73f4a3bb1e9a8a
kevin_laptop <kevin2575@yeah.net>
2013-08-06 19:58:59 +0800

    One atempt
    相关性达到0.89， 负数问题没有解决，想去掉负数的情况，但还没有把握能去掉

--- niqa/cptScore.m
+++ niqa/cptScore.m
@@ -1,4 +1,4 @@
-function [ score ] = cptScore( im )
+function [ score ] = cptScore( im,h )
 %CPTSCORE Summary of this function goes here
 %   Detailed explanation goes here
 %todo: 1.get the phase congrueny map,get the blur version of im
@@ -7,28 +7,34 @@ function [ score ] = cptScore( im )
 %todo: 3.get the normalization score
 
 pc = phasecongmono(im);
-h = fspecial('motion',10,0);
 bim = imfilter(im,h);
 
 [m n] = size(pc);
 %w = zeros(m,n);
 
-k = 5; %set size of patch to 5*5 
-t = 0.4; %threshold to take into consideration
+k = 11; %set size of patch to 5*5 
+t = 0.1; %threshold to take into consideration
 num = 0; %number of valide patches
+score = 0;
 for i = 1:k:(m-k+1)
     for j = 1:k:(n-k+1)
-        patchPC = pc(i:i+4,j:j+4);
+        patchPC = pc(i:i+k-1,j:j+k-1);
         w = getWeight(patchPC);
         if w<t
             continue;
         end
-        patchIM = im(i:i+4,j:j+4);
-        patchBIM = bim(i:i+4,j:j+4);
-        v = getVariationRatio(patchIM,patchBIM);
+        patchIM = im(i:i+k-1,j:j+k-1);
+        patchBIM = bim(i:i+k-1,j:j+k-1);
+        v = getVarRatio(patchIM,patchBIM)
+        %v = mean2(abs(patchIM - patchBIM));
         num = num+1;
-        score(num) = v*exp(w);        
+        score(num) = v*exp(w); 
+        %score(num) = v; 
     end
+end
+if num==0
+    score = 0;
+    return;
 end
 score = sum(score)/num;
 
@@ -40,20 +46,29 @@ t2 = mean(im(im<=t));
 t12 = (t1+t2)/2;
 w = mean(im(im>=t12));
 
+function r = getVarRatio(im1,im2)
+%get the ratio of variance between im1 and im2
+%todo: 1.get some data ready
+%todo: 2.get the variance of im1 and im2,respectively
+%todo: 3.compute the ratio
 
-function v = getVariationRatio(im1,im2)
-%
 [m n] = size(im1);
-s1 = 0;
-s2 = 0;
+Sh1 = 0;Sh2 = 0;
+Sv1 = 0;Sv2 = 0;
+
 for i = 1:m
     for j = 1:n-1
-        s1 = s1 + abs(im1(i,j) - im1(i,j+1));
-        s2 = s2 + abs(im2(i,j) - im2(i,j+1));
+        Sh1 = Sh1 + abs(im1(i,j) - im1(i,j+1));
+        Sh2 = Sh2 + abs(im2(i,j) - im2(i,j+1));
     end
 end
-if s1==0
-    v = 0;
-else
-    v = (s1 - s2)/(s1+0.000001);
+for i = 1:m-1
+    for j = 1:n
+        Sv1 = Sv1 + abs(im1(i,j) - im1(i+1,j));
+        Sv2 = Sv2 + abs(im2(i,j) - im2(i+1,j));
+    end
 end
+S1 = Sh1 + Sv1;
+S2 = Sh2 + Sv2;
+
+r = (S1-S2)/S1;
--- niqa/main.m
+++ niqa/main.m
@@ -1,15 +1,18 @@
-pth = 'D:\Backup\ÎÒµÄÎÄµµ\MATLAB\niqa\gblur';
+pth = 'F:\zzr\images\gblur';
 d = dir(pth);
 d = d(3:end);
 n = length(d);
 score = zeros(1,n);
+h = fspecial('gaussian',6,3);
 
+tic
 for i = 1:n
     im = imread([pth '\img' num2str(i) '.bmp']);
     im = im2double(rgb2gray(im));
-    score(i) = cptScore(im);
+    score(i) = cptScore(im,h);
     i,score(i)
 end
+toc
 save('subScores.mat','score');
 load dmos145;
-cftool(dmos145,score);
+cftool(score,100 - dmos145);
