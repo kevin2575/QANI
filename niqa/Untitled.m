fd7ce4eff293f72a41474dea70f7a4902cd82391
kevin2575 <nature2575@yeah.net>
2013-09-02 22:10:56 +0800

    改名

--- niqa/Untitled.m
+++ niqa/Untitled.m
@@ -1,68 +1,30 @@
-clear,clc
-%todo:1. prepair the data to be used later
-%todo:2. compute the orientation of gradient
-%todo:3. compute the gradient
+%todo: 1.prepair the data needed later
+%todo: 2.read one image,im_ucm,ensure the datatype
+%todo: 3.compute the gradient for the whole image,eliminate the short arrow
+%todo: 4.show the image
 
-pth = 'D:/Backup/我的文档/MATLAB/images/gblur';d = dir([pth '/*.bmp']);
-pth_gPb = 'D:/Backup/我的文档/MATLAB/images/gblur_gPb';d_gPb = dir([pth_gPb '/*.bmp']);
-pth_ucm = 'D:/Backup/我的文档/MATLAB/images/gblur_gPb_ucm';d_ucm = dir([pth_ucm '/*.bmp']);
-len = length(d_gPb);
-k = 4;%局部邻域边长
-if len~=length(d_ucm)
-    error('size not matched!');%!!!!!!!!!!!!!
+clear,clc;
+pth = 'F:/zzr/images/gblur';d = dir([pth '/*.bmp']);
+pth_gPb = 'F:/zzr/images/gblur_gPb';d_gPb = dir([pth_gPb '/*.bmp']);
+pth_ucm = 'F:/zzr/images/gblur_gPb_ucm';d_ucm = dir([pth_ucm '/*.bmp']);
+
+i = 1;
+im_ucm_name = [pth_ucm '/img' num2str(i) '.bmp'];
+%im_ucm_name = sprintf('%s/img%d.bmp',pth_ucm,i);
+im_ucm = double(imread(im_ucm_name))/255;
+if size(im_ucm,3)~=1 || max(im_ucm(:))>1 || min(im_ucm(:))<0
+    error('iamge data type not appropriate');%!!!!!!!!!!!!!!!
 end
 
-for i = 1:len
-    imName = [pth '/img' num2str(i) '.bmp'];
-    im = double(rgb2gray(imread(imName)))/255;
-    im_gPb = double(imread([pth_gPb '/img' num2str(i) '.bmp']))/255;    
-    im_ucm = double(imread([pth_ucm '/img' num2str(i) '.bmp']))/255;
-    [m,n] = size(im_ucm);
-    if (size(im,3)~=1 || min(im(:))<0 || max(im(:))>1 ||...
-        size(im_gPb,3)~=1 || min(im_gPb(:))<0 || max(im_gPb(:))>1 || ...
-        size(im_ucm,3)~=1 || min(im_ucm(:))<0 || max(im_ucm(:))>1)
-        error('data type not appropriate!');%!!!!!!!!!!!!!!!!!
-    end
-    if ~strcmp([pth '/img' num2str(i) '.bmp'] , imName)
-        error('file sequence error');%!!!!!!!!!!!!!!
-    end
-    
-    s = 0;
-    num = 0;
-    for r = k+1:m-k
-        for c = k+1:n-k
-            if im_ucm(r,c)<0.7
-                continue;
-            end
-            num=num+1;
-            rec = im_ucm(r-k:r+k,c-k:c+k);
-            %show rec!!!
-            thresh = (max(rec(:))-min(rec(:)))/2;
-            [rs,cs] = find(rec>thresh);
-            fitresult = createLinearFit(rs,cs);
-            if fitresult.p1 == 0
-                error('ratio not appliable');%!!!!!!!!
-            end
-            slope_ratio = -1/double(fitresult.p1);
-            for x = r+1:r+30
-                y = slope_ratio*x - slope_ratio*r+c;
-                y = floor(y);
-                value(x-r+1) = im_gPb(x,y);
-                if value(x-r+1)>value(x-r)
-                    break;
-                end
-            end
-            if im_gPb(r,c)-im_gPb(x,y)<0
-                %error('slope top must be bigger than bottom');%!!!!!!!!
-            end
-            s = s+(im_gPb(r,c)-im_gPb(x,y))/(sqrt(1+slope_ratio^2)*(x-r));
-        end
-    end
-    if num==0
-        error('divided by zero');%!!!!!!!!
-    end
-    score(i) = s/num;
-end
-load dmos145.mat
-plot(score,dmos145,'*')
-%score = 50 - score;
+[gx,gy] = gradient(im_ucm);
+[m,n] = size(im_ucm);
+x = 1:n;   y = m:-1:1;
+[x,y] = meshgrid(x,y);
+g = sqrt(gx.^2+gy.^2);
+selected = (g<0.4);
+x(selected) = [];
+y(selected) = [];
+gx(selected) = [];
+gy(selected) = [];
+
+quiver(x,y,gx,gy,0.5);
