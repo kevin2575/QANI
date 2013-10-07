e246fa990126f94f54e7ae78f0753d4627c9faa5
kevin2575_thinkvision <nature2575@yeah.net>
2013-10-07 22:01:43 +0800

    模糊加重

--- niqa/main.m
+++ niqa/main.m
@@ -6,14 +6,16 @@ close all;clear,clc;
 pth = 'F:/zzr/images/gblur';d = dir([pth '/*.bmp']);
 n = length(d);
 score = zeros(1,n);
-h = fspecial('gaussian',6,3);
+h = fspecial('gaussian',10,4);
 
 for j = 1:n
     im = im2double(rgb2gray(imread([pth '/img' num2str(j) '.bmp'])));
     im_blur = imfilter(im,h);
+    im_blur = imfilter(im_blur,h);
+    subplot(121),imshow(im),subplot(122),imshow(im_blur);
     
     s(j) = FeatureSIM(im,im_blur);
     j
 end
-load dmos145;
-plot(w,dmos145,'*');
+load dmos145; 
+plot(s,dmos145,'*');
