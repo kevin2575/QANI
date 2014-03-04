9f6ab84eb20ae7a8303816429f510635aa9e408a
kevin2575_thinkvision <nature2575@yeah.net>
2014-03-04 20:03:39 +0800

    。。

--- niqa/cptScore.m
+++ niqa/cptScore.m
@@ -4,18 +4,6 @@ function [ score ] = cptScore( img )
 
 im = rgb2gray(img);
 pc = phasecongmono(im);
-%figure,subplot(2,2,1),imshow(img),subplot(2,2,2),imshow(pc),subplot(2,2,3:4),imhist(pc),xlim([0 0.6]);
 score = sum(pc(:));
 [m,n] = size(im);
 score = score/(m*n);
-% pcr = roundn(pc,-2);
-% r = sort(pcr(:));
-% r = unique(r);
-% tt = pcr;
-% n = length(r);
-% for i = 1:n
-%     tt(tt==r(i)) = i/n;
-% end
-% b = pc.*tt;
-% score = sum(b(:));
-
